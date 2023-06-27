ed2269c4ef9dc27c757ad518b1bb07eecac8c93f
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-06-27 19:59:00 -0300

    Update breakout.py
    Nesse update conclui a barra do game.

--- breakout.py
+++ breakout.py
@@ -3,27 +3,33 @@ from pygame.locals import *
 
 pg.init()
 screen = pg.display.set_mode((1000,500))
-pg.display.set_caption('Dino')
+pg.display.set_caption('Breakout')
+clock = pg.time.Clock()
+pg.key.set_repeat(50)
 
 barra = pg.Surface((150,10))
 barra.fill((255,255,255))
-barra_pos_x = 450
+barra_pos = [450,470]
 
+ball = pg.Surface((10,10))
+ball.fill((255,255,255))
+ball_pos = [200,200]
 
 while True:
+    clock.tick(60)
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            rodando = False
             pg.quit()
             quit()
 
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                barra_pos_x = barra_pos_x - 10
+                barra_pos[0] -= 10 
             if event.key == K_RIGHT:
-                barra_pos_x = barra_pos_x + 10
-              
+                barra_pos[0] += 10
 
-    screen.blit(barra,(barra_pos_x, 470))
-
+    screen.fill((0,0,0))
+    screen.blit(barra,barra_pos)
+    screen.blit(ball,(200,200))
     pg.display.update()
