f07a78dc60ac7c771ef7e5e531b6a0e37a6fa03e
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-06-27 21:06:17 -0300

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -13,10 +13,17 @@ barra_pos = [450,470]
 
 ball = pg.Surface((10,10))
 ball.fill((255,255,255))
-ball_pos = [200,200]
+ball_pos = (800,450)
+UP_R = 1
+UP_L = 2 
+UP = 3
+DOWN = 4 
+DOWN_R = 5
+DOWN_L = 6
+ball_direction = UP_R
 
 while True:
-    clock.tick(60)
+    clock.tick(10)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -29,7 +36,24 @@ while True:
             if event.key == K_RIGHT:
                 barra_pos[0] += 10
 
+    if ball_direction == UP_R:
+        ball_pos = (ball_pos[0] + 10, ball_pos[1] - 10)
+    if ball_direction == UP_L:
+        ball_pos = (ball_pos[0] - 10, ball_pos[1] - 10)
+    if ball_direction == UP:
+        ball_pos = (ball_pos[0], ball_pos[1] - 10)
+    if ball_direction == DOWN_R:
+        ball_pos = (ball_pos[0] + 10, ball_pos[1] + 10)
+    if ball_direction == DOWN_L:
+        ball_pos = (ball_pos[0] - 10, ball_pos[1] + 10)
+    if ball_direction == DOWN:
+        ball_pos = (ball_pos[0], ball_pos[1] + 10)
+        
+    if ball_pos[0] > 1000 and ball_direction == UP_R:
+        ball_direction == UP_L
+        
+    print(ball_pos)
     screen.fill((0,0,0))
     screen.blit(barra,barra_pos)
-    screen.blit(ball,(200,200))
+    screen.blit(ball,ball_pos)
     pg.display.update()
